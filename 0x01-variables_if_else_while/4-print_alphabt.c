37dbb9a2f650623a25b8b3a9bfc6ff526cbd9fc5
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-19 02:30:25 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+	return (0);
+}

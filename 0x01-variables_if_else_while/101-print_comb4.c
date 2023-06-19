71f26dc77f9c965a70b8969d5cf0afcdab55216d
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-19 06:48:47 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int  digit;
+	int i;
+	int j;
+
+	for (digit = 0; digit <= 7; digit++)
+	{
+		for (i = digit + 1; i <= 8; i++)
+		{
+			for (j = i + 1; j <= 9; j++)
+			{
+				putchar(digit + '0');
+				putchar(i + '0');
+				putchar(j + '0');
+				if (digit != 7 && digit != 8 && digit != 9)
+				{
+					putchar(';');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

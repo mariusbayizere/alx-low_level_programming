1eabf6a18843b65453ddff84ada028c2027e1c52
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-19 02:49:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,12 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if (i != 'q' && i != 'e')
+		if (i == 'q' || i == 'e')
 		{
-			putchar(i);
+			continue;
 		}
-		putchar('\n');
+		putchar(i);
 	}
+	putchar('\n');
 	return (0);
 }

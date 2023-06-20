5f5dc5bd75853c8eb047ee28ac33e166f7606b05
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-20 04:53:49 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	 _putchar('\n');
+	return (0);
+}
+int _putchar(char i)
+{
+	return (putchar(i));
+}
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+}

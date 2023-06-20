ae1a3f3f7ba94c012dd4ceac4ee45afdf5f806b6
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-20 02:44:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}

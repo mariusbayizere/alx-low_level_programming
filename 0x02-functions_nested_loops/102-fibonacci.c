60f30d1cf0a5aeb9ea45ec5e83834f9e798dabb4
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-22 02:22:36 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int feb;
+	int i;
+	int n = 50;
+
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d", a);
+		feb = a + b;
+		a = b;
+		b = feb;
+		if (i != 50)
+		{
+			printf(" ,");
+		}
+	}
+	printf("\n");
+	return (0);
+}

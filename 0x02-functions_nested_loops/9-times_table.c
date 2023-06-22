97893fd8d4ab2552fd95c3a2637c27cdf3a6fc30
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-22 00:11:11 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int table;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+		table = i * j;
+
+			if (j != 0)
+				_putchar(',');
+
+			if (table < 10)
+				_putchar(' ');
+			else
+				_putchar(table / 10 + '0');
+
+			_putchar(table % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

aebc794ae31e16ecdc8e55be5fd470f6b890637e
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-23 23:27:23 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c >a )
+	else if (a > b && c >a)
 	{
 		largest = c;
 	}
-	else if (b >c)
+	else if (b > c)
 	{
 		largest = b;
 	}

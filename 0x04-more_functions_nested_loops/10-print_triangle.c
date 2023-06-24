6a0d508734a322fae70adffc3858e0cce8b619a5
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-24 06:22:50 -0700

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of '#' characters.
+ * @size: The size of the triangle.
+ *
+ * Description: This function prints a triangle of '#' characters in terminal.
+ */
+void print_triangle(int size)
+{
+	int c;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (c = 0; c < size; c++)
+	{
+		for (k = 0; k <= c; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}

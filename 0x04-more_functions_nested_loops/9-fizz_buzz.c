b3d3027a8b60360fe22f931be03d822e9749f3b3
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-24 06:02:22 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz program
+ *
+ * Description: Print number from 1 to 100,replace multiples three with "Fizz",
+ *              multiple five with "Buzz",both three and five with "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}

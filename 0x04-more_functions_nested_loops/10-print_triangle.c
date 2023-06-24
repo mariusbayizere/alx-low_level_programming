083298e6bc3d06528ccaefd17f7a23bb04fd8470
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-24 06:29:51 -0700

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,6 +18,8 @@ void print_triangle(int size)
 	}
 	for (c = 0; c < size; c++)
 	{
+		for (k = 0; k < size -c - 1; k++)
+			_putchar(' ');
 		for (k = 0; k <= c; k++)
 			_putchar('#');
 		_putchar('\n');

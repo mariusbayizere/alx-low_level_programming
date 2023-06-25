e7417332e1caa11c7228e90282e6995c8a229082
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-24 21:50:01 -0700

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,8 @@
  */
 void print_number(int n)
 {
+	unsigned long int f = n;
+
 	if (n == 0)
 	{
 		_putchar('0');
@@ -14,9 +16,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		f = -f;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (f / 10)
+		print_number(f / 10);
+	_putchar((f % 10) + '0');
 }

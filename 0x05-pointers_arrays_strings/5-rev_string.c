d351b0651757395142fdc1948ef17a1c6d3b4eff
mariusbayizere <bayizeremarius119@gmail.com>
2023-06-26 05:27:33 -0700

    5-main.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,10 +15,10 @@ void rev_string(char *s)
 	{
 		length++;
 	}
-	for (j = length -1; j >= length / 2; j--)
+	for (j = length - 1; j >= length / 2; j--)
 	{
-		temp =s[j];
-		s[j] = s[length - j -1];
-		s[length - j -1] = temp;
+		temp = s[j];
+		s[j] = s[length - j - 1];
+		s[length - j - 1] = temp;
 	}
 }

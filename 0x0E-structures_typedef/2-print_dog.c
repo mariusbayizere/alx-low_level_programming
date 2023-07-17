5b4b2e003083d04bf72a1c40c00537b7135d1f98
bayizere <bayizeremarius119@gmail.com>
2023-07-17 14:23:07 +0200

    file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,31 +9,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
-	if ((*d).name == NULL)
-	{
-		printf("Name :(nil)");
-	}
-	else
-	{
-		printf("Name:%s\n", d->name);
-	}
-	if ((*d).age < 0)
-	{
-		printf("Age:(nil)");
-	}
-	else
-	{
-		printf("Age:%f\n", d->age);
-	}
+	if (d->name == NULL)
+		d->name = "(nil)";
 	if (d->owner == NULL)
-	{
-		printf("Owner:(nil)");
-	}
-	else
-	{
-		printf("Owner:%s\n", d->owner);
-	}
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,15 +14,18 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
  * typedef struct dog dog_t;
- * The typedef statement simplifies the syntax and provides better
- * readability when working with structures.
  */
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *s);
+
 #endif
 

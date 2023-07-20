9b2422525ea4d74047d23b618500d0136f5c08fd
bayizere <bayizeremarius119@gmail.com>
2023-07-20 14:39:48 +0200

    sum-then_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -21,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(list, n);
 	for (x = 0; x < n; x++)
 	{
-		sum = sum + va_arg(list, int);
+		sum += va_arg(list, int);
 	}
 	va_end(list);
 	return (sum);

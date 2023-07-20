6637fcf3953c4e4a7800fa92f65e8ba3531fcf97
bayizere <bayizeremarius119@gmail.com>
2023-07-20 15:08:29 +0200

    1-main.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int x;
 	unsigned int sum = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
+	/*if (n == 0)*/
+	/*{*/
+		/*return (0);*/
+	/*}*/
 	va_start(list, n);
 	for (x = 0; x < n; x++)
 	{
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,29 +1,26 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
- * print_numbers - Prints number of integers separated by a given separator.
- * @separator: Pointer to a string that will be used to separate the numbers.
- * @n: The number of integer arguments to be printed.
+ * print_numbers - Prints a list of numbers, followed by a new line.
+ * @separator: The string to be printed between the numbers (or NULL).
+ * @n: The number of integer arguments passed to the function.
  * @...: The variable number of integer arguments to be printed.
+ * Return: void (no return value).
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list list;
+	va_list arg;
 	unsigned int x;
-	
 
-	va_start(list, n);
+	va_start(arg, n);
 	for (x = 0; x < n; x++)
 	{
-		va_arg(list, int);
-		printf("%d", va_arg(list, int));
-		if (x < n - 1 && separator != NULL)
-		{
+		printf("%d", va_arg(arg, int));
+		if (x != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
-	va_end(list);
+	va_end(arg);
 }

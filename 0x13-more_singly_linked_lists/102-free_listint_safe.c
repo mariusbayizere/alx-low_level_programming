3f07e0c71c06b7a8b9500d191d986ef8931d58c0
bayizere <bayizeremarius119@gmail.com>
2023-08-02 07:56:43 +0200

    free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,41 +1,53 @@
-
 #include <stdlib.h>
 #include "lists.h"
 
 /**
+ * free_list - Frees a linked list starting from a given node.
+ * @head: Pointer to the starting node.
+ * Return: The number of nodes freed.
+ */
+size_t free_list(listint_t *head)
+{
+	size_t x = 0;
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+		x++;
+	}
+	return (x);
+}
+/**
  * free_listint_safe - Frees a listint_t linked list safely.
  * @h: Pointer to the head of the linked list.
- * Return: The element of the list that was freed.
+ * Return: The number of nodes freed.
  */
-
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	int x;
-	listint_t *ptr;
+	listint_t *pointer, *ptr, *temp;
 
 	if (h == NULL || *h == NULL)
-	{
 		return (0);
-	}
-	while (*h != NULL)
+	pointer = *h;
+	ptr = (*h)->next;
+	while (ptr && ptr->next)
 	{
-		x = *h - (*h)->next;
-		if (x > 0)
+		if (pointer == ptr)
 		{
-			ptr = (*h)->next;
-			free(*h);
-			*h = ptr;
-			count++;
+			temp = pointer;
+			pointer = *h;
+			while (pointer != temp)
+			{
+				pointer = pointer->next;
+				temp = temp->next;
+			}
+			return (free_list(pointer));
 		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			count++;
-			break;
-		}
+		pointer = pointer->next;
+		ptr = ptr->next->next;
 	}
-	*h = NULL;
-	return (count);
+	return (free_list(*h));
 }

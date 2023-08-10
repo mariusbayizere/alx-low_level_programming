9e9acca195ea73739871949f6c7c1114f50f6394
bayizere <bayizeremarius119@gmail.com>
2023-08-10 17:57:55 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
  * create_file - function that creates a file.
@@ -22,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i];)
+		while (text_content[i])
 		{
 			i++;
 		}
 	}
-	temp = open("filename", O_CREAT | O_RDWR | O_TRUNC, 0600);
+	temp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	stock = write(temp, text_content, i);
 		if (temp == -1 || stock == -1)
 		{
